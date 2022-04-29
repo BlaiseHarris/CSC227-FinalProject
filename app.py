e43a8d1c7b5b4c31f6a5a6f1212aa8a66956a2f1
blaiseharris <baharris5@my.waketech.edu>
2022-04-29 13:11:36 -0700

    Added both required HTML templates.

--- app.py
+++ app.py
@@ -8,37 +8,37 @@ application = Flask(__name__)
 @application.route("/form")
 def hello():
     render_template('form.html')
-    chars = {}
-    keys_removed = (' ', '_', '\n', '-', "'", '©', '"', '%')
-    url = input("Input website you want analyzed: ")
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    print(soup.get_text())
-    for i in soup.get_text():
-        if i in chars:
-            chars[i] += 1
-        else:
-            chars[i] = 1
-    for x in keys_removed:
-        chars.pop(x, None)
-    print("Website chosen:", r.url)
-    print("------------------------------------------------------------------------------")
-    print("Characters Sorted: ", sorted(chars.items(), key=lambda kv: (kv[1], kv[0])))
+    # chars = {}
+    # keys_removed = (' ', '_', '\n', '-', "'", '©', '"', '%')
+    # url = input("Input website you want analyzed: ")
+    # r = requests.get(url)
+    # soup = BeautifulSoup(r.text, "html.parser")
+    # print(soup.get_text())
+    # for i in soup.get_text():
+    #     if i in chars:
+    #         chars[i] += 1
+    #     else:
+    #         chars[i] = 1
+    # for x in keys_removed:
+    #     chars.pop(x, None)
+    # print("Website chosen:", r.url)
+    # print("------------------------------------------------------------------------------")
+    # print("Characters Sorted: ", sorted(chars.items(), key=lambda kv: (kv[1], kv[0])))
 
-    plt.figure(figsize=(5,5))
-    graphChars = sorted(chars.items(), key=lambda kv: (kv[1], kv[0]))
-    graphChars = graphChars[-10::1]
-    x, y = zip(*graphChars)
+    # plt.figure(figsize=(5,5))
+    # graphChars = sorted(chars.items(), key=lambda kv: (kv[1], kv[0]))
+    # graphChars = graphChars[-10::1]
+    # x, y = zip(*graphChars)
 
-    plt.title("Character Amount on Page", fontsize = 20)
-    plt.xlabel("Character", fontsize=14)
-    plt.ylabel("Occured", fontsize=14)
-    plt.plot(x, y)
-    plt.show()
+    # plt.title("Character Amount on Page", fontsize = 20)
+    # plt.xlabel("Character", fontsize=14)
+    # plt.ylabel("Occured", fontsize=14)
+    # plt.plot(x, y)
+    # plt.show()
 
-@application.route('/form')
-def form():
-    return render_template('form.html')
+# @application.route('/form')
+# def form():
+#     return render_template('form.html')
  
 @application.route('/data/', methods = ['POST', 'GET'])
 def data():
