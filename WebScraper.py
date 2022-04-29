bbb9897773524df57b85fa8ea126b982d152cc21
BlaiseHarris <baharris5@my.waketech.edu>
2022-04-28 19:26:05 -0700

    Base python project from last semester.

--- WebScraper.py
+++ WebScraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     chars = {}
@@ -16,5 +17,16 @@ if __name__ == '__main__':
     for x in keys_removed:
         chars.pop(x, None)
     print("Website chosen:", r.url)
+    print("------------------------------------------------------------------------------")
     print("Characters Sorted: ", sorted(chars.items(), key=lambda kv: (kv[1], kv[0])))
 
+    plt.figure(figsize=(5,5))
+    graphChars = sorted(chars.items(), key=lambda kv: (kv[1], kv[0]))
+    graphChars = graphChars[-10::1]
+    x, y = zip(*graphChars)
+
+    plt.title("Character Amount on Page", fontsize = 20)
+    plt.xlabel("Character", fontsize=14)
+    plt.ylabel("Occured", fontsize=14)
+    plt.plot(x, y)
+    plt.show()
